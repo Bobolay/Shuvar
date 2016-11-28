11cd3f33380f8544a047fc3567d9d4649fad692f
Bobolay <bogdan4uk@gmail.com>
2016-11-28 17:27:21 +0200

    started charts

--- app/assets/javascripts/highchart.js
+++ app/assets/javascripts/highchart.js
@@ -1,25 +1,35 @@
-$(function () { 
-    var myChart = Highcharts.chart('chart-1', {
-        chart: {
-            type: 'bar'
-        },
-        title: {
-            text: 'Fruit Consumption'
-        },
-        xAxis: {
-            categories: ['Apples', 'Bananas', 'Oranges']
-        },
-        yAxis: {
-            title: {
-                text: 'Fruit eaten'
-            }
-        },
-        series: [{
-            name: 'Jane',
-            data: [1, 0, 4]
-        }, {
-            name: 'John',
-            data: [5, 7, 3]
-        }]
-    });
+$(document).ready(function(){
+
+    $chart = $('#chart-1')
+
+    if ($chart.length == 0) {
+        return;
+    }
+    else {
+        $(function () { 
+            var myChart = Highcharts.chart('chart-1', {
+                chart: {
+                    type: 'bar'
+                },
+                title: {
+                    text: 'Приклад графікуs'
+                },
+                xAxis: {
+                    categories: ['Apples', 'Bananas', 'Oranges']
+                },
+                yAxis: {
+                    title: {
+                        text: 'Fruit eaten'
+                    }
+                },
+                series: [{
+                    name: 'Jane',
+                    data: [1, 0, 4]
+                }, {
+                    name: 'John',
+                    data: [5, 7, 3]
+                }]
+            });
+        });
+    };
 });
